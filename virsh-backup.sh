a9cc695bae8c8ebb1cc4853ab4065b87a9242bf1
Walter Hüttenmeyer <drwulsen@mailbox.org>
2025-12-10 16:28:47 +0100

    Add files via upload

--- virsh-backup.sh
+++ virsh-backup.sh
@@ -1,36 +1,67 @@
 #!/bin/bash
-#set -x
-backupdir="/mnt/data/backup"
-separator="################"
+
+# virsh-backup.sh
+# Script to hot-backup running libvirt guests and cold-backup stopped ones.
+# Just set the desired backup target and run the script.
+# A subdirectory named after the corresponding domain will be created in your target directory.
+# It will dump the domains XML and the domains networks XML there,
+# then issue 'virsh backup-begin' on each domain and move the produced backup there as well.
+# For non-running machines, it will simply do the XML dumps as above, then copy the disk image(s) there.
+
+# check if we are root (works via sudo, too) - otherwise exit
+if [ "$(id -u)" -ne 0 ]; then
+  echo "Please run the backup script as root (UID 0)for proper access permissions"
+  exit 1
+fi
+
+LANG_SYS="$LANG"
+LANG="C"	# virsh domjobcomplete is later parsed as string, so language matters
+backupdir="/mnt/data/backup"	# target base directory for all domain backups
+separator="################"	# fancy decoration for output files
 declare -a domain_networks all_domains
 
 function dumpxml () {
-	virsh dumpxml "$domain" > "$domain_xml"
+	# dump domain xml to file
+	virsh dumpxml "$domain" > "$domain_xml" || quit "failed to dump domain xml for $domain"
 }
 function dumpnetxml () {
+	# dump domain network(s) xml to file
 	readarray -t domain_networks < <(virsh domiflist "$domain" | grep -i 'network' | awk ' {print $3} ')
 	for network in "${domain_networks[@]}"; do
-		virsh net-dumpxml "$network" >> "$net_xml"
+		virsh net-dumpxml "$network" >> "$net_xml" || quit "failed to dump network xml for $domain"
 	done
 }
 function virsh-backup () {
-	virsh backup-begin "$domain"
+	# start the actual backup command
+	virsh backup-begin "$domain"	# this basically forks off into the background, no return value
 }
 function domjobcomplete () {
+	# check if our backup job has finished (yet), here having set LANG is important
 	while [ "$(virsh domjobinfo "$domain" | grep 'Job type:' | awk '{ print $3 }')" != "None" ]; do
 		echo -e "Backup of domain: $domain is running...\t$(date)"
 		sleep 5
 	done
+	# lucky for us, the output of 'virsh domjobinfo domain' does reset after being printed one time
 	virsh domjobinfo --completed "$domain" >> "$logfile"
 	job_complete="$(grep --count --ignore-case --max-count=1 'completed' "${logfile}")"
 	job_isbackup="$(grep --count --ignore-case --max-count=1 'backup' "${logfile}")"
 	if [ "$job_isbackup" -gt 0 ] && [ "$job_complete" -gt 0 ];then
-		echo "Backup finished at $(date)" >> "$logfile"
+		echo "Backup of domain $domain finished at $(date)" >> "$logfile"
 	else
-		echo "Backup FAILED at $(date)" >> "$logfile"
+		echo "Backup of domain $domain FAILED at $(date)" >> "$logfile"
+		quit "Backup of domain $domain FAILED, see $logfile for more info"
 	fi
 }
+function quit () {
+	# throw a message to syslog and stderr, reset LANG and terminate
+	message="$1"
+	logger -s -p 'user.error' "$message"
+	LANG="$LANG_SYS"
+	exit 1
+}
 
+# actual control flow begins here
+# get all domain names, check which ones are running (hot backup) and which ones are off (cold backup) 
 readarray -t all_domains < <(virsh list --all | tr -s ' ' | grep -E '^ ([[:digit:]]* |- )')
 for domainline in "${all_domains[@]}"; do
 	domain="$(echo "$domainline" | tr -s ' ' | awk '{ print $2 }' )"
@@ -44,13 +75,13 @@ for domainline in "${all_domains[@]}"; do
 	dumpxml "$domain"
 	dumpnetxml "$domain"
 	if [ "$domain_active" -ne 0 ]; then
-		virsh domjobinfo "$domain" > /dev/null
+		virsh domjobinfo "$domain" > /dev/null	# clear any previous message
 		epoch="$(date +%s)"
-		epoch_trunc="${epoch::-2}"
+		epoch_trunc="${epoch::-2}"	# cut 2 digits off the epoch for later locating the right file
 		virsh-backup "$domain"
 		domjobcomplete "$domain"
 		for disk in "${disks[@]}"; do
-			moveparams=(${disk}.${epoch_trunc}* ${backupdir}/${domain}/${disk##*/})
+			moveparams=("${disk}.${epoch_trunc}"* "${backupdir}/${domain}/${disk##*/}")
 			mv "${moveparams[@]}"
 		done
 	else
