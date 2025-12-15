f9c330337b372c93f2a81692be9d6953e43ecdd5
Walter Hüttenmeyer <drwulsen@mailbox.org>
2025-12-15 11:16:06 +0100

    Update virsh-backup.sh

--- virsh-backup.sh
+++ virsh-backup.sh
@@ -6,7 +6,7 @@
 # It will dump the domains XML and the domains networks XML there,
 # then issue 'virsh backup-begin' on each domain and move the produced backup there as well.
 # For non-running machines, it will simply do the XML dumps as above, then copy the disk image(s) there.
-#set -x
+set -x
 LANG_SYS="$LANG"
 LANG="C"	# virsh domjobcomplete is later parsed as string, so language matters
 backupdir="/mnt/data/backup"	# target base directory for all domain backups
@@ -80,6 +80,7 @@ function moveimages () {
 			log "ERROR: Error moving file ${disk}.${timestamp_trunc}\* to ${backupdir}/${domain}/${disk##*/}/" 'log'
 			return 1
 		else
+			log "SUCCESS: Moved ${disk}.${timestamp_trunc}\* to ${backupdir}/${domain}/${disk##*/}/" 'log'
 			return 0
 		fi
 	done
